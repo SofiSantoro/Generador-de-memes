83c9e08887459226c7deb3c07cb34d4eb7d740fa
SofiSantoro <sofiaasantoro@gmail.com>
2021-08-11 00:09:51 -0300

    se agrega modo oscuro

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//PANELES//
+//CONSTANTES//
 
 const barraLateralImagen = document.getElementById("panelImagen")
 
@@ -8,7 +8,17 @@ const botonDeAperturaBarraImagen = document.getElementById("botonImagen")
 
 const mostrarBarraLateralImagen = document.getElementById("panelImagen")
 
+const mostrarImagenMeme = document.getElementById("imagenDeMeme")
 
+const inputImagen = document.getElementById("inputImagen")
+
+const color = document.getElementById("header")
+const botonModoOscuro = document.getElementById("botonModoOscuro")
+const botonModoClaro = document.getElementById("botonModoClaro")
+const botonTexto = document.getElementById("botonTexto")
+
+
+//PANELES//
 
 const ocultarPanel = () => {
     barraLateralImagen.style.display = "NONE"
@@ -20,3 +30,41 @@ const mostrarPanel = () => {
     barraLateralImagen.style.display = "inline"
 }
 botonDeAperturaBarraImagen.onclick = mostrarPanel
+
+
+//IMAGEN DE MEME//
+
+// const imagen = () => {
+//     mostrarImagenMeme.src = ""
+// }
+// inputImagen.onclick = imagen
+
+
+
+//MODO OSCURO//
+
+const modoOscuro = () => {
+    color.style.background = "black"
+    botonDeAperturaBarraImagen.style.background = "gray"
+    botonTexto.style.background = "gray"
+    barraLateralImagen.style.background = "gray"
+    botonDecierreImagen.style.background = "black"
+    botonModoClaro.style.display = "inline"
+    botonModoClaro.style.background = "gray"
+    botonModoOscuro.style.display = "none"
+}
+botonModoOscuro.onclick = modoOscuro
+
+const modoClaro = () => {
+    color.style.background = "rgb(236, 179, 72)"
+    botonDeAperturaBarraImagen.style.background = "rgb(236, 179, 72)"
+    botonTexto.style.background = "rgb(236, 179, 72)"
+    barraLateralImagen.style.background = "palevioletred"
+    botonDecierreImagen.style.background = "palevioletred"
+    botonDecierreImagen.style.color = "wheat"
+    botonModoClaro.style.display = "none"
+    botonModoOscuro.style.background = "rgb(236, 179, 72)"
+    botonModoOscuro.style.display = "inline"
+}
+botonModoClaro.onclick = modoClaro
+
